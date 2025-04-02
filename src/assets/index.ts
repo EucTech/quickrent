509793b9ee8ceb4be9c083f88bb08a31a20020b8
EucTech <euccreative@gmail.com>
2025-04-02 16:38:27 -0400

    added images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,35 @@
-import logo from "./images/logo.png"
+import logo from "./images/logo.png";
+import house1 from "./images/house1.jpg";
+import house2 from "./images/house2.jpg";
+import house3 from "./images/house3.jpg";
+import house4 from "./images/house4.jpg";
+import house5 from "./images/house5.jpg";
+import house6 from "./images/house6.jpg";
+import house7 from "./images/house7.jpg";
+import house8 from "./images/house8.jpg";
+import house9 from "./images/house9.jpg";
+import house10 from "./images/house10.jpg";
+import house11 from "./images/house11.jpg";
+import house12 from "./images/house12.jpg";
+import house13 from "./images/house13.jpg";
+import house14 from "./images/house14.jpg";
 
 
 export const Images = { 
-    logo 
+    logo,
+    house1,
+    house2,
+    house3,
+    house4,
+    house5,
+    house6,
+    house7,
+    house8,
+    house9,
+    house10,
+    house11,
+    house12,
+    house13,
+    house14,
+
 }
