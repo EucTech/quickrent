1e7279f60ce7bf225196da3200e3055bd1fea8ba
EucTech <euccreative@gmail.com>
2025-04-02 16:43:47 -0400

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import FindByLocation from "@/components/HomePage/FindByLocation";
+import HeroPage from "@/components/HomePage/HeroPage";
+import PropertiesSection from "@/components/HomePage/PropertiesSection";
 
 export default function Home() {
   return (
-    <div>
-        <h1>Home</h1>
+    <div className="w-full">
+        <HeroPage/>
+        <PropertiesSection/>
+        <FindByLocation/>
     </div>
   );
 }
---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PricingPage = () => {
+  return (
+    <div className='  '>
+      Pricing
+    </div>
+  )
+}
+
+export default PricingPage
