ed38e916cbc22109ce6af2bf87809620fe5c156e
EucTech <euccreative@gmail.com>
2025-04-06 15:59:43 -0400

    added featured component

--- src/components/HomePage/FeaturedPropertyProps.tsx
+++ src/components/HomePage/FeaturedPropertyProps.tsx
@@ -3,11 +3,9 @@ import { Bath, BedDouble, LocateFixed, MapPin } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
-import { Navigation } from "swiper/modules";
-import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+
 
 interface FeaturedPropertyType {
   listingType: string;
